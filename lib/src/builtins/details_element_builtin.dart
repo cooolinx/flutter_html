0c0fda58c0d1605a144d1e0b7ced5aa2fe94dbec
Colin <fallen_lord@126.com>
2025-08-14 13:44:45 +0800

    refact: details tag for better performance and visual

--- lib/src/builtins/details_element_builtin.dart
+++ lib/src/builtins/details_element_builtin.dart
@@ -1,6 +1,127 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 
+class _DetailsWidget extends StatefulWidget {
+  final Map<StyledElement, InlineSpan> childList;
+  final Iterable<InlineSpan> children;
+  final Style style;
+
+  const _DetailsWidget({
+    super.key,
+    required this.childList,
+    required this.children,
+    required this.style,
+  });
+
+  @override
+  State<_DetailsWidget> createState() => _DetailsWidgetState();
+}
+
+class _DetailsWidgetState extends State<_DetailsWidget> {
+  bool isExpanded = false;
+
+  Style get styleWithTextOnly => Style(
+    color: widget.style.color,
+    fontSize: widget.style.fontSize,
+    fontWeight: widget.style.fontWeight,
+    fontStyle: widget.style.fontStyle,
+  );
+
+  EdgeInsets get margin => EdgeInsets.only(
+    left: widget.style.margin?.left?.value ?? 0,
+    right: widget.style.margin?.right?.value ?? 0,
+    top: widget.style.margin?.top?.value ?? 0,
+    bottom: widget.style.margin?.bottom?.value ?? 0,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    if (!isExpanded) {
+      return Container(
+        margin: margin,
+        width: double.infinity,
+        child: _buildToggle(),
+      );
+    }
+
+    final direction = widget.style.direction ?? Directionality.maybeOf(context);
+    final padding = widget.style.padding?.resolve(direction!);
+
+    return Container(
+      width: double.infinity,
+      margin: margin,
+      padding: padding,
+      decoration: BoxDecoration(
+        color: widget.style.backgroundColor,
+        borderRadius: widget.style.borderRadius?.toBorderRadius() ?? BorderRadius.zero,
+        border: widget.style.border,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          _buildToggle(),
+          SizedBox(height: widget.style.padding?.top?.value ?? 0),
+          if (isExpanded) CssBoxWidget.withInlineSpanChildren(
+            children: widget.childList.keys.isNotEmpty &&
+                    widget.childList.keys.first.name == "summary"
+                ? widget.children.skip(1).toList()
+                : widget.children.toList(),
+            style: styleWithTextOnly,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildToggle() {
+    String? text;
+    if (widget.childList.keys.isNotEmpty && widget.childList.keys.first.name == "summary") {
+      // Extract plain text from inline span to avoid occupying whole row width
+      text = _extractTextFromInlineSpan(widget.children.first);
+    }
+    final title = Text(
+      text ?? "Details",
+      // Keep only text styles because the rest of the styles are applied to the container
+      style: styleWithTextOnly.generateTextStyle(),
+    );
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          isExpanded = !isExpanded;
+        });
+      },
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          title,
+          SizedBox(width: (widget.style.padding?.left?.value ?? 0) * 0.5),
+          AnimatedRotation(
+            turns: isExpanded ? 0.5 : 0.0,
+            duration: const Duration(milliseconds: 200),
+            child: Icon(
+              Icons.keyboard_arrow_down_rounded,
+              size: widget.style.fontSize?.value,
+              color: widget.style.color,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  String? _extractTextFromInlineSpan(InlineSpan inlineSpan) {
+    if (inlineSpan is! WidgetSpan || inlineSpan.child is! CssBoxWidget) return null;
+    final cssBox = inlineSpan.child as CssBoxWidget;
+    if (cssBox.child is! Text) return null;
+    final textWidget = cssBox.child as Text;
+    if (textWidget.textSpan == null) return null;
+    final span = textWidget.textSpan!;
+    if (span is! TextSpan) return null;
+    return span.toPlainText();
+  }
+}
+
 /// The [DetailsElementBuiltIn] handles the default rendering for the
 /// `<details>` html tag
 class DetailsElementBuiltIn extends HtmlExtension {
@@ -8,46 +129,34 @@ class DetailsElementBuiltIn extends HtmlExtension {
 
   @override
   Set<String> get supportedTags => {
-        "details",
-      };
+    "details",
+  };
 
   @override
   StyledElement prepare(
       ExtensionContext context, List<StyledElement> children) {
     return StyledElement(
       name: context.elementName,
+      elementId: context.id,
+      elementClasses: context.classes.toList(),
       children: children,
       style: Style(),
       node: context.node,
     );
   }
 
-  @override
+    @override
   InlineSpan build(ExtensionContext context) {
     final childList = context.builtChildrenMap!;
     final children = childList.values;
 
-    InlineSpan? firstChild = children.isNotEmpty ? children.first : null;
     return WidgetSpan(
-      child: ExpansionTile(
-          key: AnchorKey.of(context.parser.key, context.styledElement!),
-          expandedAlignment: Alignment.centerLeft,
-          title: childList.keys.isNotEmpty &&
-                  childList.keys.first.name == "summary"
-              ? CssBoxWidget.withInlineSpanChildren(
-                  children: firstChild == null ? [] : [firstChild],
-                  style: context.styledElement!.style,
-                )
-              : const Text("Details"),
-          children: [
-            CssBoxWidget.withInlineSpanChildren(
-              children: childList.keys.isNotEmpty &&
-                      childList.keys.first.name == "summary"
-                  ? children.skip(1).toList()
-                  : children.toList(),
-              style: context.styledElement!.style,
-            ),
-          ]),
+      child: _DetailsWidget(
+        key: AnchorKey.of(context.parser.key, context.styledElement!),
+        childList: childList,
+        children: children,
+        style: context.styledElement!.style,
+      ),
     );
   }
 }
